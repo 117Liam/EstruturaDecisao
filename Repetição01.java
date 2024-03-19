40b4396bfb3adf1111b6c4761dc6c968f39e58b7
117Liam <164023705+117Liam@users.noreply.github.com>
2024-03-19 11:20:54 -0300

    Add files via upload

---
+++ Repetição01.java
@@ -0,0 +1,21 @@
+// Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int nota;
+
+    System.out.println("Digite uma nota: ");
+    nota = input.nextInt();
+
+    while (nota > 10 || nota < 0) {
+      if (nota <= 10 && nota >= 0) {
+        System.out.println("Nota válida");
+      } else {
+        System.out.println("Nota inválida. Digite novamente");
+      }
+      System.out.println("Digite uma nota: ");
+      nota = input.nextInt();
+    }   
+  }
+}
