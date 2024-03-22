fc2aa880b05d013c3b62945cfaa85bf89f9d61d1
117Liam <164023705+117Liam@users.noreply.github.com>
2024-03-22 09:56:16 -0300

    Repetição01.java

--- Repetição01.java
+++ Repetição01.java
@@ -1,4 +1,3 @@
-// Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido
 import java.util.Scanner;
 public class Repetição01 {
   public static void main(String[] args) {
