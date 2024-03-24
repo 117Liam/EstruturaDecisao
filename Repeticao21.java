255f938fe0e6d222056de395e04a4ca58fc85362
117Liam <164023705+117Liam@users.noreply.github.com>
2024-03-24 17:45:30 -0300

    Add files via upload

---
+++ Repeticao21.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Repeticao21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro para verificar se é primo: ");
+        int numero = scanner.nextInt();
+
+        boolean primo = verificarPrimo(numero);
+
+        if (primo) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
+    }
+
+    public static boolean verificarPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
