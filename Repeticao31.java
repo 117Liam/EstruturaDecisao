169051a18bea08e7e33ee5cafe048112892bb78b
117Liam <164023705+117Liam@users.noreply.github.com>
2024-03-24 18:54:13 -0300

    Repeticao31.java

--- Repeticao31.java
+++ Repeticao31.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Repeticao30 {
+public class Repeticao31 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int produtoNumero = 1;
