d7b325a6a0234e203b4e4f507405ab3b6d3c5578
117Liam <164023705+117Liam@users.noreply.github.com>
2024-03-24 16:50:45 -0300

    Add files via upload

---
+++ Repeticao07.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Repeticao07 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int num, maior = 0;
+    for(int i = 0; i < 5; i++){
+      System.out.println("Digite um número: ");
+      num = input.nextInt();
+      if(num > maior){
+        maior = num;
+      }
+    }
+    System.out.println("O maior número é: " + maior);
+
+    input.close();
+    
+  }
+    
+ 
+}
