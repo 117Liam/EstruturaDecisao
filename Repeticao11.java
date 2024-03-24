5e91298d82e9398f2e851c990839bf053ab27bb8
117Liam <164023705+117Liam@users.noreply.github.com>
2024-03-24 17:00:29 -0300

    Add files via upload

---
+++ Repeticao11.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Repeticao11 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número inteiro: ");
+        int numero1 = leitor.nextInt();
+
+        System.out.print("Digite o segundo número inteiro: ");
+        int numero2 = leitor.nextInt();
+
+        System.out.println("Números no intervalo entre " + numero1 + " e " + numero2 + ":");
+
+        int menor = Math.min(numero1, numero2);
+        int maior = Math.max(numero1, numero2);
+        int soma = 0;
+
+        for (int i = menor; i <= maior; i++) {
+            System.out.println(i);
+            soma += i;
+        }
+
+        System.out.println("Soma dos números: " + soma);
+
+        leitor.close();
+    }
+}
