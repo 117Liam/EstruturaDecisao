2edb15655e00e5a8b95040da1a257bc7bce279a2
117Liam <164023705+117Liam@users.noreply.github.com>
2024-03-24 18:52:46 -0300

    Add files via upload

---
+++ Repeticao30.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Repeticao30 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int produtoNumero = 1;
+        double totalCompra = 0;
+
+        System.out.println("Lojas Tabajara");
+
+        while (true) {
+            System.out.print("Produto " + produtoNumero + ": R$ ");
+            double preco = scanner.nextDouble();
+
+            if (preco == 0) {
+                break; 
+            }
+
+            totalCompra += preco;
+            produtoNumero++;
+        }
+
+        System.out.println("Total: R$ " + totalCompra);
+
+        System.out.print("Dinheiro: R$ ");
+        double dinheiro = scanner.nextDouble();
+
+        double troco = dinheiro - totalCompra;
+        System.out.println("Troco: R$ " + troco);
+    }
+}
